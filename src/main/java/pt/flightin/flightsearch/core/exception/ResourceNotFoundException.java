4b74d309f85d7c7d3a573c10f1e465aa0ce43a38
Rui Barbosa <rui.barbosa@celfocus.com>
2021-04-15 22:37:23 +0100

    Remove redundant access modifier from ResourceNotFoundException

--- src/main/java/pt/flightin/flightsearch/core/exception/ResourceNotFoundException.java
+++ src/main/java/pt/flightin/flightsearch/core/exception/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package pt.flightin.flightsearch.core.exception;
 
 public class ResourceNotFoundException extends BaseException {
 
-    private static final String MSG = "%s not found";
+    static final String MSG = "%s not found";
 
     public ResourceNotFoundException(String message) {
         super(String.format(MSG, message));
